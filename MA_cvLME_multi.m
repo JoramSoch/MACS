JoramSoch/MACS

commit e72c9472568d3c42ecfd1e051afa8b84449e668c

optimized cvLME calculation
With this commit, the routine for cvLME estimation was optimized. The second posterior (after applying the first posterior from N-1 session to the 1 remaining session) is the same for all CV folds, there it is only calculated once. (Edit on 01/12/2017, Commit on 24/12/2017)

MA_cvLME_multi.m
@@ -67,7 +67,7 @@ function MA_cvLME_multi(SPM, data, mode, AnC)
 % E-Mail: joram.soch@bccn-berlin.de
 % 
 % First edit: 04/03/2014, 19:00 (V0.1/V1)
-%  Last edit: 24/05/2017, 09:45 (V1.0/V16)
+%  Last edit: 01/12/2017, 00:25 (V1.1/V17)
 
 
 %=========================================================================%
@@ -208,7 +208,6 @@ for h = 1:s
     S(h).X = X(S(h).n,S(h).p);  % design matrix
     S(h).P = P(S(h).n,S(h).n);  % precision matrix
 end;
-clear Y X P
 
 
 %=========================================================================%
@@ -225,6 +224,13 @@ m0_ni = zeros(p(1),1);          % flat Gaussian
 L0_ni = exp(-23)*eye(p(1));
 a0_ni = 0;                      % Jeffrey's prior
 b0_ni = 0;
+
+% Estimate posteriors from all data
+%-------------------------------------------------------------------------%
+if strcmp(mode,'N-1')
+    [mn_all, Ln_all, an_all, bn_all] = ME_GLM_NG(Y, X, P, m0_ni, L0_ni, a0_ni, b0_ni, sprintf('Estimate posteriors over all sessions 1-%d',s));
+end;
+clear Y X P
 
 % Cycle through cross-validation folds
 %-------------------------------------------------------------------------%
@@ -248,23 +254,15 @@ for g = 1:s
         % Set non-informative priors
         %-----------------------------------------------------------------%
         m0 = m0_ni; L0 = L0_ni; a0 = a0_ni; b0 = b0_ni;
-
+        
         % Estimate posteriors for this fold
         %-----------------------------------------------------------------%
         [mn, Ln, an, bn] = ME_GLM_NG(Yf, Xf, Pf, m0, L0, a0, b0, sprintf('Estimate posteriors that serve as priors for session %d',g));
 
-        % Set these as informative priors
-        %-----------------------------------------------------------------%
-        m0 = mn; L0 = Ln; a0 = an; b0 = bn;
-
-        % Estimate posteriors for remaining session
-        %-----------------------------------------------------------------%
-        [mn, Ln, an, bn] = ME_GLM_NG(S(g).Y, S(g).X, S(g).P, m0, L0, a0, b0, sprintf('Estimate posteriors for session %d using priors from other sessions',g));
-        
         % Save priors and posteriors for this session
         %-----------------------------------------------------------------%
-        S(g).m0 = m0; S(g).L0 = L0; S(g).a0 = a0; S(g).b0 = b0;
-        S(g).mn = mn; S(g).Ln = Ln; S(g).an = an; S(g).bn = bn;
+        S(g).m0 = mn;     S(g).L0 = Ln;     S(g).a0 = an;     S(g).b0 = bn;
+        S(g).mn = mn_all; S(g).Ln = Ln_all; S(g).an = an_all; S(g).bn = bn_all;
         clear m0 L0 a0 b0 mn Ln an bn
 
     end;

MA_cvLME_single.m
@@ -61,7 +61,7 @@ function MA_cvLME_single(SPM, data, disc, AnC)
 % E-Mail: joram.soch@bccn-berlin.de
 % 
 % First edit: 04/03/2014, 19:00 (V0.1/V1)
-%  Last edit: 24/05/2017, 09:45 (V1.0/V16)
+%  Last edit: 01/12/2017, 00:35 (V1.1/V17)
 
 
 %=========================================================================%
@@ -204,7 +204,6 @@ if length(s1) == length(s2)
     n1 = numel(s1);             % data points
     n2 = numel(s2);
 end;
-clear Y X P
 
 
 %=========================================================================%
@@ -222,29 +221,18 @@ L0 = exp(-23)*eye(p);
 a0 = 0;                         % Jeffrey's prior
 b0 = 0;
 
-% Estimate posteriors for 1st part (as priors for 2nd part)
+% Estimate (informative) posteriors from all data
+%-------------------------------------------------------------------------%
+[mn, Ln, an, bn] = ME_GLM_NG(Y, X, P, m0, L0, a0, b0, 'Estimate posteriors over both parts');
+clear Y X P
+
+% Estimate posteriors from 1st part (as priors for 2nd part)
 %-------------------------------------------------------------------------%
 [mn1, Ln1, an1, bn1] = ME_GLM_NG(Y1, X1, P1, m0, L0, a0, b0, 'Estimate 1st part posteriors (as 2nd part priors)');
 
-% Estimate posteriors for 2nd part (as priors for 1st part)
+% Estimate posteriors from 2nd part (as priors for 1st part)
 %-------------------------------------------------------------------------%
 [mn2, Ln2, an2, bn2] = ME_GLM_NG(Y2, X2, P2, m0, L0, a0, b0, 'Estimate 2nd part posteriors (as 1st part priors)');
-
-% Set (informative) priors for both parts
-%-------------------------------------------------------------------------%
-m01 = mn2;  m02 = mn1;          % normal distribution
-L01 = Ln2;  L02 = Ln1;
-a01 = an2;  a02 = an1;          % gamma distribution
-b01 = bn2;  b02 = bn1;
-clear mn* Ln* an* bn*
-
-% Estimate posteriors for 1st part (with priors from 2nd part)
-%-------------------------------------------------------------------------%
-[mn1, Ln1, an1, bn1] = ME_GLM_NG(Y1, X1, P1, m01, L01, a01, b01, 'Estimate 1st part posteriors (with 2nd part priors)');
-
-% Estimate posteriors for 2nd part (with priors from 1st part)
-%-------------------------------------------------------------------------%
-[mn2, Ln2, an2, bn2] = ME_GLM_NG(Y2, X2, P2, m02, L02, a02, b02, 'Estimate 2nd part posteriors (with 1st part priors)');
 
 
 %=========================================================================%
@@ -268,19 +256,19 @@ end;
 
 % Calculate evidence for 1st part (using estimates from 2nd part)
 %-------------------------------------------------------------------------%
-oosLME1(m_ind) = ME_GLM_NG_LME(P1, L01, a01, b01, Ln1, an1, bn1);
+oosLME1(m_ind) = ME_GLM_NG_LME(P1, Ln2, an2, bn2, Ln, an, bn);
 
 % Calculate accuracy and complexity for 1st part (using 2nd part)
 %-------------------------------------------------------------------------%
-if AnC, [oosAcc1(m_ind), oosCom1(m_ind)] = ME_GLM_NG_AnC(X1, P1, m01, L01, a01, b01, mn1, Ln1, an1, bn1, 'Compute accuracy and complexity for 1st part'); end;
+if AnC, [oosAcc1(m_ind), oosCom1(m_ind)] = ME_GLM_NG_AnC(X1, P1, mn2, Ln2, an2, bn2, mn, Ln, an, bn, 'Compute accuracy and complexity for 1st part'); end;
 
 % Calculate evidence for 2nd part (using estimates from 1st part)
 %-------------------------------------------------------------------------%
-oosLME2(m_ind) = ME_GLM_NG_LME(P2, L02, a02, b02, Ln2, an2, bn2);
+oosLME2(m_ind) = ME_GLM_NG_LME(P2, Ln1, an1, bn1, Ln, an, bn);
 
 % Calculate accuracy and complexity for 2nd part (using 1st part)
 %-------------------------------------------------------------------------%
-if AnC, [oosAcc2(m_ind), oosCom2(m_ind)] = ME_GLM_NG_AnC(X2, P2, m02, L02, a02, b02, mn2, Ln2, an2, bn2, 'Compute accuracy and complexity for 2nd part'); end;
+if AnC, [oosAcc2(m_ind), oosCom2(m_ind)] = ME_GLM_NG_AnC(X2, P2, mn1, Ln1, an1, bn1, mn, Ln, an, bn, 'Compute accuracy and complexity for 2nd part'); end;
 
 % Calculate total model evidence (assuming independence between parts)
 %-------------------------------------------------------------------------%

commit fbd20131e4caccbc98cda47b0cc628a667781494

corrected earlier mistake
With this change, a mistake introduced in an earlier commit (e72c9472568d3c42ecfd1e051afa8b84449e668c, 24/12/2017) was corrected. For some unknown reason, one line that should have come with this commit was not in the code. Now it is.

MA_cvLME_multi.m
@@ -67,7 +67,7 @@ function MA_cvLME_multi(SPM, data, mode, AnC)
 % E-Mail: joram.soch@bccn-berlin.de
 % 
 % First edit: 04/03/2014, 19:00 (V0.1/V1)
-%  Last edit: 16/02/2018, 13:15 (V1.2/V18)
+%  Last edit: 19/02/2018, 12:15 (V1.2/V18)
 
 
 %=========================================================================%
@@ -237,6 +237,7 @@ b0_ni = 0;
 % Estimate posteriors from all data
 %-------------------------------------------------------------------------%
 if strcmp(mode,'N-1')
+    X = vertcat(S(1:s).X);
     [mn_all, Ln_all, an_all, bn_all] = ME_GLM_NG(Y, X, P, m0_ni, L0_ni, a0_ni, b0_ni, sprintf('Estimate posteriors over all sessions 1-%d',s));
 end;
 clear Y X P